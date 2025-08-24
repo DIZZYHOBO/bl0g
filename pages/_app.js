7997a75bdabbdca60ce73c6f2e86804fe21271c4
DIZZYHOBO <dystorp@gmail.com>
2025-08-24 22:17:12 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
+import { AuthProvider } from '../hooks/useAuth';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <AuthProvider>
       <span className="theme-bejamas" />
       <Component {...pageProps} />
-    </>
+    </AuthProvider>
   );
 }
 
